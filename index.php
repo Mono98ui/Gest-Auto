239a5ab4a368ed9157bbf4f45cea1d504a8f45d9
Charles M-P <charlesmartinezpham2022@outlook.com>
2024-01-20 14:28:39 -0500

    Modification to Index.php and new files
    Now the website as a css style, some image, a nav bar and a footer, need some cleaning on the code thought

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
+<?php include("./header_and_footer/header.html"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css" />  
+  <link rel="shortcut icon" href="path/to/your/favicon.ico" /> <!-- Specify the path to your favicon file -->
+  <title>Home</title>
 </head>
 <body>
-    <h1>PRICES :</h1>
-    <h2>pizza : 18.67$</h2>
-    <form action="index.php" method="post">
-        <label> How many pizza do you want ?</label><br>
-        <input type="text" name="prices"><br>
-        <input type="submit" values="total">
-    </form>
+    <main>
+        <h1 class="modified-header">
+            Welcome to <mark class="blue">Our Garage</mark>, your hassle-free solution for tire changes.
+        </h1>
+        <p id="modified-paragraph">Whether it's a compact car or a class 2 truck, enjoy our fast and professional services.
+            Schedule your appointment now!</p>
+        
+            <a href="appointment.php" class="btn btn-schedule">Schedule an Appointment</a> <!-- Link to your appointment scheduling page -->
+    </main>
 </body>
 </html>
-<?php
-    $price = 18.67;
-    $total = $_POST["prices"] * $price;
-    echo "Ur total is : \${$total};";
-?>
+<?php include("./header_and_footer/footer.html"); ?>
