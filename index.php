eb1fa5034339144989dfad5c4a6d12c20ff96cda
Charles M-P <charlesmartinezpham2022@outlook.com>
2024-01-20 13:55:18 -0500

    testing code
    Testing php code

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<php?>
-    echo "Hello world !"
-</php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PRICES :</h1>
+    <h2>pizza : 18.67$</h2>
+    <form action="index.php" method="post">
+        <label> How many pizza do you want ?</label><br>
+        <input type="text" name="prices"><br>
+        <input type="submit" values="total">
+    </form>
+</body>
+</html>
+<?php
+    $price = 18.67;
+    $total = $_POST["prices"] * $price;
+    echo "Ur total is : \${$total};";
+?>
